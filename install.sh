eb146aaa3bbe109e12d13dacb3fafc6f7c48506e
liuxiao <appleeye.zju@gmail.com>
2015-08-06 18:13:04 +0800

    first commit

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#/*===============================================================
+# @Author: liuxiao
+# @Created Time : Wed 29 Jul 2015 06:28:17 PM CST
+#
+# @File Name: install.sh
+# @Description:
+#
+#================================================================*/
+#
+
+ln  ~/.vim/.vimrc  ~/.vimrc
+ln  ~/.vim/.tmux.conf  ~/.tmux.conf
+cp  ~/.vim/.bashrc  ~/.bashrc
---
+++ scritps/loginAWS.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+echo "login AWS server: please choose which one to login"
+echo "1: dev tomcat server"
+echo "2: uat tomcat server"
+echo "3: comcast task server"
+echo "4: api.everstring.com"
+echo "5: prod controller"
+echo "6: uat score-job"
+echo "7: poc-api"
+echo "8: BeiJing local"
+echo "9:Azure API Dev"
+echo "10:My own Azure: the password is the same as Apple ID"
+read  input
+case $input in
+1)  ssh -i   ~/.ssh/liuxiao                 xiaoliu@ec2-54-91-162-79.compute-1.amazonaws.com;; 
+2)  ssh -i   ~/.ssh/liuxiao                 xiaoliu@23.22.26.23;;
+3)  ssh -i   ~/.ssh/liuxiao                 xiaoliu@54.225.32.27;;
+4)  ssh -i   ~/.ssh/liuxiao                 xiaoliu@api.everstring.com;;
+5)  ssh -i   ~/.ssh/liuxiao                 xiaoliu@54.89.191.150;;
+6)  ssh -i   ~/.ssh/liuxiao                 xiaoliu@uat.everstring.com;;
+7)  ssh -i   ~/.ssh/liuxiao                 xiaoliu@54.89.207.30;;
+8)  ssh -i      ~/.ssh/liuxiao                 xiaoliu@192.168.0.10 ;; 
+9)  ssh -i   ~/data/keys/Azure.key          es@esdev-api.cloudapp.net;;
+10) ssh      azureuser@appleeye.chinacloudapp.cn;;
+esac
+
